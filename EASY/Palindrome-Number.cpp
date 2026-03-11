5afafe7242bf84f20178e965193be8554fe14a71
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-03-11 18:19:31 +0530

    Create Palindrome-Number.cpp

---
+++ EASY/Palindrome-Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int rev=0,n;
+        n=x;
+        while(x>0){
+            rev = (rev*10) + x%10;
+            x = x/10;
+        }
+        if(n==rev) return true;
+        else return false;
+    }
+};
