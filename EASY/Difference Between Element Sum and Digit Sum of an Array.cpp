09169d6f719caaaa46d8c9e166ea623ac4de9c27
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-05-17 10:55:22 +0530

    Create Difference Between Element Sum and Digit Sum of an Array.cpp

---
+++ EASY/Difference Between Element Sum and Digit Sum of an Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int differenceOfSum(vector<int>& nums) {
+        int sum1 = 0,sum2 = 0,divi;     
+
+        for(int i=0;i<nums.size();i++){
+            sum1 = sum1 + nums[i];
+
+            while(nums[i]>9){
+                divi = nums[i]%10;
+                sum2 = sum2 + divi;
+                nums[i] /= 10;
+            }
+            sum2 = sum2 + nums[i];
+       }
+
+       return abs(sum1-sum2);
+    }
+};
