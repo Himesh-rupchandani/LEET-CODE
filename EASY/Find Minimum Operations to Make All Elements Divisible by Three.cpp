c4d666d305495d5dfa92f264ea0af90d742fc873
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-05-21 17:36:20 +0530

    Create Find Minimum Operations to Make All Elements Divisible by Three.cpp

---
+++ EASY/Find Minimum Operations to Make All Elements Divisible by Three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%3==1){
+                nums[i] = nums[i] - 1;
+                cnt++;
+            } 
+
+            else if(nums[i]%3==2){
+                nums[i] = nums[i] + 1;
+                cnt++;
+            } 
+        }
+        return cnt;
+    }
+};
