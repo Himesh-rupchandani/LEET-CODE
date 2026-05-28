0f9cca18c327713614a5c350a95a60e7e82c804b
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-05-28 07:33:41 +0530

    Create Final Array State After K Multiplication Operations I.cpp

---
+++ EASY/Final Array State After K Multiplication Operations I.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        while(k!=0){
+
+        int mini = 0;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]<nums[mini]) mini = i;
+            }
+
+            nums[mini] = nums[mini]*multiplier;
+            k--;
+        }
+
+        return nums;
+    }
+};
