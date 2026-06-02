a011d1c5aaa9fd10927e1ac4d6f0db380d3b8de3
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-06-02 08:35:51 +0530

    Create Average Value of Even Numbers That Are Divisible by Three.cpp

---
+++ EASY/Average Value of Even Numbers That Are Divisible by Three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int averageValue(vector<int>& nums) {
+        int sum = 0,cnt = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%3==0 && nums[i]%2==0){
+                sum += nums[i];
+                cnt++;
+            }
+        }
+
+        if(cnt==0) return sum;
+        else return sum/cnt;
+    }
+};
