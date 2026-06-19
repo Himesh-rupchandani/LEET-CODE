d559294d8d28a78ce21970c3c3921d7667d4cdc1
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-06-19 18:24:50 +0530

    Update Valid Anagram.cpp

--- EASY/Valid Anagram.cpp
+++ EASY/Valid Anagram.cpp
@@ -1,3 +1,5 @@
+// Brute force
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -6,5 +8,29 @@ public:
 
         if(s==t) return true;
         return false;
+    }
+};
+
+//Optimal
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // if length's are not Equal.
+        if(s.size()!=t.size()) return false;
+        vector<int> frq(26,0); 
+
+        // Store frequancy of character in s and t.
+        for(int i=0;i<s.size();i++){
+            frq[s[i] - 'a']++;
+            frq[t[i] - 'a']--;
+        }
+        
+        // Checking if frequancy of any character is zero.
+        for(int i=0;i<26;i++){
+            if(frq[i]!=0) return false; //not anagram.
+        }
+
+        return true;
     }
 };
