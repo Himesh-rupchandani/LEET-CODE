656a407fed2d9e1a85d5f3d0a61025a91129259d
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-06-26 10:58:29 +0530

    Create Lemonade Change.cpp

---
+++ EASY/Lemonade Change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int cnt5 = 0,cnt10 = 0;
+
+        for(int i=0;i<bills.size();i++){
+
+            if(bills[i]==5) cnt5++;
+
+            else if(bills[i]==10){
+                cnt10++;
+                cnt5--;
+                if(cnt5<0) return false;
+            }
+            
+            else{
+                if(cnt10<1) cnt5 -= 3;
+                else{
+                    cnt10--;
+                    cnt5--;
+                }
+                if(cnt5<0 || cnt10<0) return false;
+            }
+        }
+
+        return true;
+    }
+};
