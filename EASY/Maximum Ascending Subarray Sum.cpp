115350f82bb1860e238f687ed380be0d93869372
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-07-12 12:43:26 +0530

    Create Maximum Ascending Subarray Sum.cpp

---
+++ EASY/Maximum Ascending Subarray Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum = nums[0],max_sum = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1]) sum += nums[i];
+            else{
+                max_sum = max(max_sum,sum);
+                sum = nums[i];
+            }
+        }
+        return max(max_sum,sum);   
+    }
+};
