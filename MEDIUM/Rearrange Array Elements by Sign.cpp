7a01c061edfcda79baca259015c6dd2fb3e54b70
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-07-28 17:20:17 +0530

    Update Rearrange Array Elements by Sign.cpp

--- MEDIUM/Rearrange Array Elements by Sign.cpp
+++ MEDIUM/Rearrange Array Elements by Sign.cpp
@@ -1,3 +1,5 @@
+// High ms code
+
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
@@ -28,6 +30,48 @@ public:
                         j++;
                     }
                     ans.push_back(nums[j]);
+                    j++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+// Low ms code
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size(),0);
+
+        for(int i=0,j=0,k=0;k<nums.size();k++){
+            if(k%2==0){
+                if(nums[i] > 0){
+                    ans[k] = nums[i];
+                    i++;
+                }
+                else{
+                    while(nums[i] < 0){
+                        i++;
+                    }
+                    ans[k] = nums[i];
+                    i++;
+                }
+            }
+
+            else{
+                if(nums[j] < 0){
+                    ans[k] = nums[j];
+                    j++;
+                }
+                else{
+                    while(nums[j] > 0){
+                        j++;
+                    }
+                    ans[k] = nums[j];
                     j++;
                 }
             }
